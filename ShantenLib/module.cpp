dbd3d14c7c9cf964deb3b213b358c4d48c23d596
colin-PC <colinlee10@gmail.com>
2020-01-26 17:43:10 -0500

    remove tutorial stuff

--- ShantenLib/module.cpp
+++ ShantenLib/module.cpp
@@ -6,28 +6,9 @@
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
 
-const double e = 2.7182818284590452353602874713527;
-
-double sinh_impl(double x) {
-    return (1 - pow(e, (-2 * x))) / (2 * pow(e, -x));
-}
-
-double cosh_impl(double x) {
-    return (1 + pow(e, (-2 * x))) / (2 * pow(e, -x));
-}
-
-double tanh_impl(double x) {
-    std::vector<int> test;
-    test.at(-1);
-    return sinh_impl(x) / cosh_impl(x);
-}
-
 namespace py = pybind11;
 
 PYBIND11_MODULE(ShantenLib, m) {
-    m.def("fast_tanh2", &tanh_impl, R"pbdoc(
-        Compute a hyperbolic tangent of a single argument expressed in radians.
-    )pbdoc");
     py::class_<ShantenCalculator::ImprovementCount>(m, "ImprovementCount")
         .def(py::init())
         .def_readwrite("Neg", &ShantenCalculator::ImprovementCount::Neg)
